e46d2fbc68211640c96b545fe0bbbeddf68667c9
song song <ssong@arxanculture.com>
2018-06-22 19:53:11 +0800

    完善接口

--- api/wallet.php
+++ api/wallet.php
@@ -7,17 +7,21 @@ interface WalletApi {
     // 注册钱包
     function register($register_body,&$response);
     // 获取钱包的基本信息
-  //  function getWalletInfo();
+    //function getWalletInfo();
     // 创建数字资产
-   function createPOE($poe_body,$sign_body,&$response);
+    function createPOE($poe_body,$sign_body,&$response);
     // 上传数字资产凭证
-   // function uploadPOEFile();
+    function uploadPOEFile($asset_id,$file,$mode,&$response);
     // 发行资产
-  //  function issuerAsset();
+    function issuerAsset($asset_body,$sign_body,&$response);
+    // 发行token
+    function issuerCToken($ctoken_body,$sign_body,&$response);
     // 转让资产
-   // function transferAsset();
+    function transferAsset($transfer_body,$sign_body,&$response);
+    // 转移ctoken
+    function transferCToken($transfer_body,$sign_body,&$response);
     // 交易历史
-   // function tranfserTxn();
+    function tranfserTxn($id,$mode,$response);
 }
 
 class WalletClient implements WalletApi {
@@ -41,9 +45,9 @@ class WalletClient implements WalletApi {
 
         // 设置http请求头
         $header = array();
-        $header[] = 'API-Key:' . $api_key;
-        $header[] = 'Content-Type: application/json;charset=utf-8'; 
-        $header[] = 'Bc-Invoke-Mode:sync';
+        $header[0] = 'API-Key:' . $api_key;
+        $header[1] = 'Content-Type: application/json;charset=utf-8'; 
+        $header[2] = 'Bc-Invoke-Mode:sync';
         curl_setopt($this->curl_client, CURLOPT_HTTPHEADER, $header);
         curl_setopt($this->curl_client, CURLOPT_RETURNTRANSFER, 1);        
     }
@@ -109,7 +113,232 @@ class WalletClient implements WalletApi {
         return 0;
     }
 
-    /*
+    // TODO 上传存证文件
+    function uploadPOEFile($asset_id,$file,$mode,&$response){
+        // 打开文件，读文件
+        $str = file_get_contents($file);
+        // 组装form数据
+        $data = array(
+            "poe_id"=>$asset_id,
+            "poe_file"=>"$file",
+            "read_only"=>"$mode",
+        );
+        // 通道加密签名
+        $this->ecc_client->signAndEncrypt($data,$request);
+        
+        // 设置http请求
+        // 这个请求与其他的请求数据不同，为了方便，在此重新设置一个新的客户端，并在使用完毕后，销毁
+        $upload_curl = curl_init();
+        $header = array();
+        $header[0] = 'API-Key:' . $this->api_key;
+        $header[1] = 'Content-Type:multipart/form-data';
+        //$header[1] = 'Content-Type: application/json;charset=utf-8'; 
+        $header[2] = 'Bc-Invoke-Mode:sync';
+
+        echo "header:\n";
+        var_dump($header);
+
+        $url = $this->host . "/wallet-ng/v1/poe/upload";
+        curl_setopt($upload_curl, CURLOPT_URL, $url);
+
+        curl_setopt($upload_curl, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($upload_curl, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($upload_curl, CURLOPT_POSTFIELDS, $request);
+
+        // 发送请求
+        $res = curl_exec($upload_curl);
+        if ($res == ""){
+            echo "curl error" ,"\n";
+            return -1;
+        }
+
+        echo $res , "\n";
+
+        // 加密与验签
+        $this->ecc_client->decryptAndVerify($res,$data);
+        if (empty($data)){
+            echo "data empty\n";
+            echo "decrypt_and_verify error" , "\n"; 
+            return -2;
+        }
+
+        $response = $data;
+        curl_close($upload_curl);
+        return 0;
+    }
+
+    // 发行资产
+    function issuerAsset($asset_body,$sign_body,&$response){
+        // 创建ed25519对象
+        $this->sign_client = new Signature($sign_body);
+
+        // 签名
+        $this->sign_client->sign($asset_body,$signed_data);
+
+        // 销毁签名对象
+        unset($this->sign_client); 
+        $this->sign_client = NULL;
+
+        // 签名返回的数据，交给ecc去加密签名
+        $this->ecc_client->signAndEncrypt($signed_data,$request);
+
+        // 发送请求
+        curl_setopt($this->curl_client, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($this->curl_client, CURLOPT_POSTFIELDS, $request);
+        $url = $this->host . "/wallet-ng/v1/transaction/assets/issue";
+        curl_setopt ($this->curl_client, CURLOPT_URL, $url);
+
+        $res = curl_exec($this->curl_client);
+        if ($res == ""){
+            echo "curl error" ,"\n";
+            return -1;
+        }
+
+        $this->ecc_client->decryptAndVerify($res,$data);
+        if (empty($data)){
+            echo "decrypt_and_verify error" , "\n"; 
+            return -2;
+        }
+
+        $response = $data;
+        return 0;
+    }
+
+    // 发行token
+    function issuerCToken($ctoken_body,$sign_body,&$response){
+        // 创建ed25519对象
+        $this->sign_client = new Signature($sign_body);
+
+        // 签名
+        $this->sign_client->sign($ctoken_body,$signed_data);
+
+        // 销毁签名对象
+        unset($this->sign_client); 
+        $this->sign_client = NULL;
+
+        // 签名返回的数据，交给ecc去加密签名
+        $this->ecc_client->signAndEncrypt($signed_data,$request);
+
+        // 发送请求
+        curl_setopt($this->curl_client, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($this->curl_client, CURLOPT_POSTFIELDS, $request);
+        $url = $this->host . "/wallet-ng/v1/transaction/tokens/issue";
+        curl_setopt ($this->curl_client, CURLOPT_URL, $url);
+
+        $res = curl_exec($this->curl_client);
+        if ($res == ""){
+            echo "curl error" ,"\n";
+            return -1;
+        }
+
+        $this->ecc_client->decryptAndVerify($res,$data);
+        if (empty($data)){
+            echo "decrypt_and_verify error" , "\n"; 
+            return -2;
+        }
+
+        $response = $data;
+        return 0;
+    }
+
+    // 转让资产
+    function transferAsset($transfer_body,$sign_body,&$response){
+        // 创建ed25519对象
+        $this->sign_client = new Signature($sign_body);
+
+        // 签名
+        $this->sign_client->sign($transfer_body,$signed_data);
+
+        // 销毁签名对象
+        unset($this->sign_client); 
+        $this->sign_client = NULL;
+
+        // 签名返回的数据，交给ecc去加密签名
+        $this->ecc_client->signAndEncrypt($signed_data,$request);
+
+        // 发送请求
+        curl_setopt($this->curl_client, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($this->curl_client, CURLOPT_POSTFIELDS, $request);
+        $url = $this->host . "/wallet-ng/v1/transaction/assets/transfer";
+        curl_setopt ($this->curl_client, CURLOPT_URL, $url);
+
+        $res = curl_exec($this->curl_client);
+        if ($res == ""){
+            echo "curl error" ,"\n";
+            return -1;
+        }
+
+        $this->ecc_client->decryptAndVerify($res,$data);
+        if (empty($data)){
+            echo "decrypt_and_verify error" , "\n"; 
+            return -2;
+        }
+
+        $response = $data;
+        return 0;
+
+    }
+
+    // 转让token
+    function transferCToken($transfer_body,$sign_body,&$response){
+        // 创建ed25519对象
+        $this->sign_client = new Signature($sign_body);
+
+        // 签名
+        $this->sign_client->sign($transfer_body,$signed_data);
+
+        // 销毁签名对象
+        unset($this->sign_client); 
+        $this->sign_client = NULL;
+
+        // 签名返回的数据，交给ecc去加密签名
+        $this->ecc_client->signAndEncrypt($signed_data,$request);
+
+        // 发送请求
+        curl_setopt($this->curl_client, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($this->curl_client, CURLOPT_POSTFIELDS, $request);
+        $url = $this->host . "/wallet-ng/v1/transaction/tokens/transfer";
+        curl_setopt($this->curl_client, CURLOPT_URL, $url);
+
+        $res = curl_exec($this->curl_client);
+        if ($res == ""){
+            echo "curl error" ,"\n";
+            return -1;
+        }
+
+        $this->ecc_client->decryptAndVerify($res,$data);
+        if (empty($data)){
+            echo "decrypt_and_verify error" , "\n"; 
+            return -2;
+        }
+
+        $response = $data;
+        return 0;
+    }
+
+    function tranfserTxn($id,$type,$response){
+        //发送get请求
+        $url = $this->host . "/wallet-ng/v1/transaction/logs?did=" . $did . "&type = " . $type;
+        curl_setopt($this->curl_client, CURLOPT_URL, $url);
+
+        $res = curl_exec($this->curl_client);
+        if ($res == ""){
+            echo "curl error" ,"\n";
+            return -1;
+        }
+
+        // 验签解密
+        $this->ecc_client->decryptAndVerify($res,$data);
+        if (empty($data)){
+            echo "decrypt_and_verify error" , "\n"; 
+            return -2;
+        }
+
+        $response = $data;
+        return 0;
+    }
+
+  /*
     function getWalletInfo(){
 
     }
@@ -117,15 +346,6 @@ class WalletClient implements WalletApi {
     function uploadPOEFile(){
 
     }
-
-    function issuerAsset(){
-
-    }
-
-    function transferAsset(){
-
-    }
-
     function tranfserTxn(){
 
     }
